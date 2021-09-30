b181e2bf146355d03478db73f752dbc677abd454
borgesgfj <borgesgfj@gmail.com>
2021-09-30 16:27:01 -0300

    Make item editable  - Description:    Was add a fetaure that allows user to edit some item on list by click    on edit button. When edit button is clicked it fires a function that    set the item properties inEdition to be true and renders inputs fields    at the correspondent table rows.  - Main changes:    - Functions writeItemOnTable and writeQttyOnTable was renamed to      cellOfItemsContent and cellOfQttyContent in order to have a name      more close to their abstractions;    - Function cellOfItemsContent and cellOfQttyContent renders inputs      elements in table row relatated to an item if such item inEdition      state is true;    - Create a function editAndConfirmEditionBtns that creates a edit      button in a table row if the correspondent item inEdition state is      false, otherwise is created a button to confirm edition;    - The edit button calls the function makeItemEditable that change the      inEdition state of the  item realted with the parent row of the  button      which calls it. After that it calls renderTableOfItems functions;    - The renderTableOfItems functions delete the table cell of delete      button in a row that contains a item in edition and make the      quantity cell span 2 columns to better acommodate the select input      group.    - The button that confirm edition calls the function confirmEdition      when clicked. The confirmEdition function take the inputs values      and rewrite item properties    - The inputs of rows in edition calls functions to validadate both,      empty input text and duplicated values.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const arrayOfItems = [];
 addButton.disabled = true;
 const qtty = document.getElementById('inputQtty');
 qtty.value = '1';
-function validatEmptyInputs() {
+function validateEmptyInputs() {
   const item = document.getElementById('inputItem').value;
   addButton.disabled = item.trim() === '';
 }
-document.getElementById('inputItem').addEventListener('change', validatEmptyInputs);
+document.getElementById('inputItem').addEventListener('change', validateEmptyInputs);
 
 function validateDuplicatedItem() {
   const newItem = document.getElementById('inputItem').value.trim();
@@ -37,10 +37,9 @@ function renderTableOfItems(tableBodyId) {
   const tableBody = document.getElementById(tableBodyId);
   tableBody.innerHTML = '';
   for (let i = 0; i < arrayOfItems.length; i++) {
-    if (!arrayOfItems[i].inEdition) {
-      let itemCellClass = 'intensCell align-middle';
-      let qttyCellClass = 'align-middle qttyCellStandard';
-      let tableRowClass = 'standardRow';
+    let itemCellClass = 'intensCell align-middle';
+    let qttyCellClass = 'align-middle qttyCellStandard';
+    let tableRowClass = 'standardRow';
       if (arrayOfItems[i].acquired) {
         itemCellClass += ' checkedTd';
         qttyCellClass += ' checkedTd';
@@ -49,7 +48,7 @@ function renderTableOfItems(tableBodyId) {
       tableBody.innerHTML += `
         <tr class="${tableRowClass}">
           <td class= "${itemCellClass}" onclick="markAcquiredItem(this.parentElement)">
-            ${writeItemOnTable(
+            ${cellOfItemsContent(
               arrayOfItems[i].item,
               arrayOfItems[i].itemDescription,
               arrayOfItems[i].acquired,
@@ -57,7 +56,7 @@ function renderTableOfItems(tableBodyId) {
             )}
           </td>
           <td class= "${qttyCellClass}">
-            ${writeQttyOnTable(
+            ${cellOfQttyContent(
               arrayOfItems[i].quantity,
               arrayOfItems[i].unity,
               arrayOfItems[i].inEdition
@@ -66,23 +65,26 @@ function renderTableOfItems(tableBodyId) {
           <td class="align-middle">
             ${createDeleteBtn()}
           </td>
-          <td class="align-middle"> </td>
+          <td class="align-middle">${editAndConfirmEditionBtns(arrayOfItems[i].inEdition, arrayOfItems[i].acquired)} </td>
         </tr>
       `;
-    }
+      if(arrayOfItems[i].inEdition) {
+        tableBody.parentElement.rows[i +1].deleteCell(2)
+        tableBody.parentElement.rows[i +1].cells[1].colSpan = 2
+      }
   }
 }
-function writeItemOnTable(itemName, descripionOfItem, acquiredState, editState) {
+function cellOfItemsContent(itemName, descripionOfItem, acquiredState, editState) {
   if (!editState) {
     let classOfCheckMarkContainer = 'tickContainer';
-    let itemTextLeftIdent = 'standardLeftIdent';
+    let itemtextLeftIdent = 'standardLeftIdent';
     if (acquiredState) {
       classOfCheckMarkContainer += ' tickContainerChecked';
-      itemTextLeftIdent += ' checkedIdent';
+      itemtextLeftIdent += ' checkedIdent';
     }
     return `
       <div class="${classOfCheckMarkContainer}">
-        <div class="${itemTextLeftIdent}">
+        <div class="${itemtextLeftIdent}">
           <p class="itemName">
             ${itemName} <br/>
             <span class="itemsDescription" >
@@ -92,25 +94,70 @@ function writeItemOnTable(itemName, descripionOfItem, acquiredState, editState) 
         </div>
       </div>
     `;
+  } else {
+    return `
+      <div class="input-group-vertical input-group-sm mt-1">
+        <input
+          type="text"
+          class="form-control"
+          name="inputEditItemName"
+          id="inputEditItemName"
+          placeholder="Editar nome do produto"
+          oninput="validateDuplicateEditInputs(this.offsetParent.parentElement)"
+          value = ${itemName}
+        />
+        <input
+        type="text"
+        class="form-control ms-2 mt-1"
+        name="inputEditItemDescription"
+        id="inputEditItemDescription"
+        placeholder="Editar descrição do produto"
+        value = ${descripionOfItem}
+        />
+      </div>
+    `;
   }
 }
-function writeQttyOnTable(qtty, unitySymbol, editState) {
+function cellOfQttyContent(qtty, unitySymbol, editState) {
   if (!editState) {
     return `
       <p class="qttyParagraph">
         ${qtty}
-        <span class="unityText">
+        <span class="unitytext">
           ${unitySymbol}
         </span>
       </p>
     `;
+  } else{
+    return `
+      <div class="input-group input-group-sm flex-nowrap">
+        <input
+          type="number"
+          class="form-control-sm inputEditQtty"
+          name="EditQtty"
+          id="EditQtty"
+          min="1"
+          size="4"
+          value=${qtty}
+        />
+        <select class="form-select form-select-sm selectEditUnity"
+          id="selectInputUnity"
+        >
+          <option value="un.">un.</option>
+          <option value="kg">kg</option>
+          <option value="g">g</option>
+          <option value="Duz">Duz</option>
+          <option value="1/2 Duz">1/2 Duz</option>
+        </select>
+      </div>
+    `
   }
 }
 function createDeleteBtn() {
   return `
     <button
       type="button"
-      class="btn btn-danger d-flex align-items-center justify-content-center"
+      class="btn btn-danger d-flex align-items-center justify-content-center smallTableBtn"
       id="delBtn"
       onclick="removeItem(this)"
     >
@@ -124,14 +171,68 @@ function removeItem(deleteBtnReference) {
   document.getElementById('listTable').deleteRow(indexOfRow);
   arrayOfItems.splice(indexOfRow - 1, 1);
 }
-function createEditBttn(rowReference) {
-  const cellEdit = rowReference.insertCell();
-  cellEdit.id = 'editCell';
-  cellEdit.className = 'align-middle';
+function editAndConfirmEditionBtns(editState, acquiredState) {
+  if (acquiredState) {
+    return ''
+  }
+  if (!editState) {
+    return `
+    <button
+      type="button"
+      class="btn btn-primary d-flex align-items-center justify-content-center smallTableBtn"
+      id="editBtn"
+      onclick="makeItemEditable(this.parentElement.parentElement)"
+    >
+      <span class="bi bi-pencil-square tableBtnIcon"></span>
+    </button>
+    `;
+  }
+  else {
+    return `
+      <button
+        type="button"
+        class="btn btn-success d-flex align-items-center justify-content-center smallTableBtn"
+        id="confirmEditionBtn"
+        onclick="confirmEdition(this.parentElement.parentElement)"
+      >
+        <span class="bi bi bi-check-circle-fill tableBtnIcon"></span>
+      </button>
+    `;
+  }
 }
 
 function markAcquiredItem(rowReference) {
-  arrayOfItems[rowReference.rowIndex - 1].acquired =
+  if(!arrayOfItems[rowReference.rowIndex - 1].inEdition) {
+    arrayOfItems[rowReference.rowIndex - 1].acquired =
     !arrayOfItems[rowReference.rowIndex - 1].acquired;
+    renderTableOfItems('listTableBody');
+  }
+}
+function validateEmptyEditionInputs() {
+  const editedItemName = document.getElementById('inputEditItemName').value
+  document.getElementById('confirmEditionBtn').disabled = editedItemName.trim() === ''
+}
+
+function validateDuplicateEditInputs(rowReference) {
+  const productIndex = rowReference.rowIndex -1
+  const editedItemName = document.getElementById('inputEditItemName').value
+  document.getElementById('confirmEditionBtn').disabled = arrayOfItems.some((products, index) => {
+    if (index != productIndex) {
+      return products.item === editedItemName.trim()
+    }
+  })
+}
+function makeItemEditable(rowReference) {
+  arrayOfItems[rowReference.rowIndex - 1].inEdition = !arrayOfItems[rowReference.rowIndex - 1].inEdition;
   renderTableOfItems('listTableBody');
+  document.getElementById('inputEditItemName').addEventListener('change', validateEmptyEditionInputs)
+}
+
+function confirmEdition(rowReference) {
+  arrayOfItems[rowReference.rowIndex - 1].item = document.getElementById('inputEditItemName').value
+  arrayOfItems[rowReference.rowIndex - 1].itemDescription = document.getElementById('inputEditItemDescription').value
+  arrayOfItems[rowReference.rowIndex - 1].quantity = document.getElementById('EditQtty').value
+  arrayOfItems[rowReference.rowIndex - 1].unity = document.getElementById('selectInputUnity').value
+  arrayOfItems[rowReference.rowIndex - 1].inEdition = false
+  renderTableOfItems('listTableBody')
 }
