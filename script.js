4dbd48612c28305b1a8de878b182bc5e6d7dff0a
Gilberto Borges <57193296+borgesgfj@users.noreply.github.com>
2021-09-30 11:13:47 -0300

    Refactor code (#10)
    * Refactor code-02
    Description: A code refactor was perfomed in order to change the logic
      of renderization to be determined by items properties states. The main
    changes are:
    - Use innerHTML with HTML sintax to create elements as: buttons, table
      and texts;
    - Create a function renderTableItems() that render the table rows according to
      the acquired and inEdition boolean properties of each item, where the CSS classes are passed as state dependent 
     variables
    - Attributes a onclick event to table cells wich contains the item name
      and description. This events calls the function markAcquiredItem() and
    pass a reference to the parent row of item;
    - Change the functions writeItemOnTable() and writeQttyOnTable() to
      render the text information according to the item state.

--- script.js
+++ script.js
@@ -15,68 +15,108 @@ function validateDuplicatedItem() {
 }
 document.getElementById('inputItem').addEventListener('change', validateDuplicatedItem);
 
-function addElement(tableReference) {
+function addElement() {
   const newProduct = {
     item: document.getElementById('inputItem').value.trim(),
     itemDescription: document.getElementById('inputItemDescription').value.trim(),
     quantity: document.getElementById('inputQtty').value,
     unity: document.getElementById('selectUnity').value,
     acquired: false,
+    inEdition: false,
   };
   arrayOfItems.push(newProduct);
-  const newRowIndex = insertNewRow(tableReference).rowIndex;
-  const currentRowRef = document.getElementById(tableReference).rows[newRowIndex];
-  writeItemOnTable(currentRowRef, newProduct.item, newProduct.itemDescription);
-  writeQttyOnTable(currentRowRef, newProduct.quantity, newProduct.unity);
-  createDeltBttn(currentRowRef);
-  createEditBttn(currentRowRef);
+  renderTableOfItems('listTableBody');
   addButton.disabled = true;
-}
-
-function insertNewRow(tableID) {
-  const table = document.getElementById(tableID);
-  const newRow = table.tBodies[0].insertRow(-1);
-  return newRow;
-}
-function writeItemOnTable(rowReference, itemName, descripionOfItem) {
-  const cellOfitens = rowReference.insertCell(0);
-  cellOfitens.id = 'intensCell';
-  cellOfitens.innerHTML = `<div class="tickContainer">
-    <div class="cellTextContent">
-      <p class="itemName">${itemName} <br/>
-        <span class="itemsDescription" >
-          ${descripionOfItem}
-        </span>
-      </p>
-    </div>
-  </div>`;
   document.getElementById('inputItem').value = '';
   document.getElementById('inputItemDescription').value = '';
-}
-function writeQttyOnTable(rowReference, qtty, unitySymbol) {
-  const cellOfQtty = rowReference.insertCell();
-  cellOfQtty.className = 'align-middle';
-  cellOfQtty.id = 'qttyCells';
-  cellOfQtty.innerHTML = `${qtty} ${unitySymbol}`;
   document.getElementById('inputQtty').value = '1';
   document.getElementById('selectUnity').value = 'un.';
 }
-function createDeltBttn(rowReference) {
-  const cellDelete = rowReference.insertCell();
-  const delBtn = document.createElement('button');
-  const delIcon = document.createElement('span');
-  cellDelete.id = 'deleteCell';
-  cellDelete.className = 'align-middle';
-  delIcon.id = 'delIcon';
-  delIcon.className = 'bi bi-journal-x';
-  delBtn.appendChild(delIcon);
-  delBtn.type = 'button';
-  delBtn.className = 'btn btn-danger d-flex align-items-center justify-content-center';
-  delBtn.id = 'delBtn';
-  delBtn.onclick = function () {
-    removeItem(this);
-  };
-  cellDelete.appendChild(delBtn);
+
+function renderTableOfItems(tableBodyId) {
+  const tableBody = document.getElementById(tableBodyId);
+  tableBody.innerHTML = '';
+  for (let i = 0; i < arrayOfItems.length; i++) {
+    if (!arrayOfItems[i].inEdition) {
+      let itemCellClass = 'intensCell align-middle';
+      let qttyCellClass = 'align-middle qttyCellStandard';
+      let tableRowClass = 'standardRow';
+      if (arrayOfItems[i].acquired) {
+        itemCellClass += ' checkedTd';
+        qttyCellClass += ' checkedTd';
+        tableRowClass += ' checkedRow';
+      }
+      tableBody.innerHTML += `
+        <tr class="${tableRowClass}">
+          <td class= "${itemCellClass}" onclick="markAcquiredItem(this.parentElement)">
+            ${writeItemOnTable(
+              arrayOfItems[i].item,
+              arrayOfItems[i].itemDescription,
+              arrayOfItems[i].acquired,
+              arrayOfItems[i].inEdition
+            )}
+          </td>
+          <td class= "${qttyCellClass}">
+            ${writeQttyOnTable(
+              arrayOfItems[i].quantity,
+              arrayOfItems[i].unity,
+              arrayOfItems[i].inEdition
+            )}
+          </td>
+          <td class="align-middle">
+            ${createDeleteBtn()}
+          </td>
+          <td class="align-middle"> </td>
+        </tr>
+      `;
+    }
+  }
+}
+function writeItemOnTable(itemName, descripionOfItem, acquiredState, editState) {
+  if (!editState) {
+    let classOfCheckMarkContainer = 'tickContainer';
+    let itemTextLeftIdent = 'standardLeftIdent';
+    if (acquiredState) {
+      classOfCheckMarkContainer += ' tickContainerChecked';
+      itemTextLeftIdent += ' checkedIdent';
+    }
+    return `
+      <div class="${classOfCheckMarkContainer}">
+        <div class="${itemTextLeftIdent}">
+          <p class="itemName">
+            ${itemName} <br/>
+            <span class="itemsDescription" >
+              ${descripionOfItem}
+            </span>
+          </p>
+        </div>
+      </div>
+    `;
+  }
+}
+function writeQttyOnTable(qtty, unitySymbol, editState) {
+  if (!editState) {
+    return `
+      <p class="qttyParagraph">
+        ${qtty}
+        <span class="unityText">
+          ${unitySymbol}
+        </span>
+      </p>
+    `;
+  }
+}
+function createDeleteBtn() {
+  return `
+    <button
+      type="button"
+      class="btn btn-danger d-flex align-items-center justify-content-center"
+      id="delBtn"
+      onclick="removeItem(this)"
+    >
+      <span class="bi bi-journal-x" id="delIcon"></span>
+    </button>
+  `;
 }
 
 function removeItem(deleteBtnReference) {
@@ -90,16 +130,8 @@ function createEditBttn(rowReference) {
   cellEdit.className = 'align-middle';
 }
 
-function markAcquiredItem(event) {
-  if (event.target.tagName == 'P') {
-    const outterCellContainer = event.target.parentNode.parentNode;
-    const cellOfQtty = event.target.parentNode.parentNode.parentNode.nextSibling;
-    const clickedRow = event.target.parentNode.parentNode.parentNode.parentNode;
-    const clickedRowIndex = clickedRow.rowIndex;
-    outterCellContainer.classList.toggle('tickContainerChecked');
-    clickedRow.classList.toggle('checkedRow');
-    cellOfQtty.classList.toggle('checkedTd');
-    arrayOfItems[clickedRowIndex - 1].acquired = clickedRow.className == 'checkedRow';
-  }
+function markAcquiredItem(rowReference) {
+  arrayOfItems[rowReference.rowIndex - 1].acquired =
+    !arrayOfItems[rowReference.rowIndex - 1].acquired;
+  renderTableOfItems('listTableBody');
 }
-document.getElementById('listTable').addEventListener('click', markAcquiredItem);
